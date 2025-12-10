b16a7eef21cbe44753ccfacfd026256e4200c03d
Ryan Attard <ryanattard@gmail.com>
2025-12-10 10:49:05 +0100

    Simple Querying

---
+++ SWD61A_OOP_2025_2026/Week6_Common/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6_Common
+{
+    public class Test
+    {
+        protected internal string Name { get; set;}
+    }
+}
--- SWD61A_OOP_2025_2026/Week6_Presentation/Program.cs
+++ SWD61A_OOP_2025_2026/Week6_Presentation/Program.cs
@@ -67,7 +67,7 @@ namespace Week6_Presentation
                 Console.Clear();
                 Console.WriteLine(" ============== Student menu ============== ");
                 Console.WriteLine("1. List all students");
-                Console.WriteLine("2. List by group");
+                Console.WriteLine("2. List students by group");
                 Console.WriteLine("3. Search for student");
                 Console.WriteLine("4. Add");
 
@@ -90,6 +90,51 @@ namespace Week6_Presentation
                         Console.ReadKey();
 
                         break;
+
+                    case 2:
+
+                        //ask the user to input the group name
+                        Console.WriteLine("Input the group name");
+                        string inputGroupName = Console.ReadLine();
+
+                        //var list = studentsRepository.GetByGroupName(...)
+                        var list2 = studentsRepository.GetByGroup(inputGroupName);
+
+                        //display the list of students returned from the above line
+
+                        foreach (var student in list2)
+                        {
+                            Console.WriteLine($"{student.Name}\t\t{student.Surname}\t\t -\t {student.Id}");
+                        }
+
+                        Console.WriteLine("Click any button to return back to Students Menu...");
+                        Console.ReadKey();
+
+                        break;
+
+                    case 3:
+                        //ask the user to input the group name
+                        Console.WriteLine("Input keyword");
+                        string inputKeyword = Console.ReadLine();
+
+                        //var list = studentsRepository.GetByGroupName(...)
+                        var list3 = studentsRepository.Get(inputKeyword);
+
+                        //display the list of students returned from the above line
+
+                        foreach (var student in list3)
+                        {
+                            Console.WriteLine($"{student.Name}\t\t{student.Surname}\t\t -\t {student.Id}");
+                        }
+
+                        Console.WriteLine("Click any button to return back to Students Menu...");
+                        Console.ReadKey();
+
+
+                        break;
+
+
+
 
                     case 10:
                         Console.WriteLine("Press any key to go back to the main menu...");
